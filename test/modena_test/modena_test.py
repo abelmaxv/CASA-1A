21271afa0c68b844b4106ffa551753766432db03
Abel Verley <abelmaxv@gmail.com>
2024-07-11 12:05:09 +0100

    format modena test

--- test/modena_test/modena_test.py
+++ test/modena_test/modena_test.py
@@ -1,10 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import osmnx as ox
 from os import path, makedirs
 from percolation.percolation import Percolation
 from src.linkage_tree import LinkageTree
 from src.cluster import Clustering
-import osmnx as ox
+from src.condensed_tree import CondensedTree
 
 # Importing the modena model
 print("Importing the network ... \n")
@@ -17,10 +18,10 @@ if path.isfile(network_path):
 else : 
     print("Creating the network ...")
     import data.data_queries.modena_query
-    print("Network created. \n")
-    dtype = {"id":int}
+    print("Network created... \n ")
     G = ox.load_graphml(network_path, node_dtypes= node_dtypes)
     G = G.to_undirected()
+
 
 print(f"\nSize of the model : {G.number_of_nodes()} \n")
 
@@ -28,7 +29,7 @@ print(f"\nSize of the model : {G.number_of_nodes()} \n")
 print("Displaying the graph ... \n ")
 network_display_path = "test/modena_test/modena_network.png"
 if not(path.isfile(network_display_path)):
-    ox.plot.plot_graph(G, node_size = 2, node_color = '#3F4A99', edge_color = "#B0B0B0", bgcolor = '#FFFFFF', show = False, save = True, filepath = network_display_path, close = True)
+   ox.plot.plot_graph(G, node_size = 2, node_color = '#3F4A99', edge_color = "#B0B0B0", bgcolor = '#FFFFFF', show = False, save = True, filepath = network_display_path, close = True)
 else: 
     print("Graph had already been plotted. \n")
 
@@ -39,7 +40,7 @@ print("\n \n \n")
 
 # Generate the percolation tree :
 print("Generating the percolation tree ... \n")
-tree_path = "data/data/percolation_trees/modena_tree.csv"
+tree_path = "data/data/percolation_trees/modena_tree.npy"
 if path.isfile(tree_path):
     print("Percolation tree had already been computed. \n")
     percolation_tree = LinkageTree(path = tree_path)
@@ -54,7 +55,7 @@ else :
 print("Displaying the percolation tree ... \n")
 tree_display_path = "test/modena_test/modena_percolation_tree.png"
 if not(path.isfile(tree_display_path)): 
-    percolation_tree.plot(truncate_mode='level', p= 10)
+    percolation_tree.plot(truncate_mode="level", p=10)
     plt.title("Percolation tree of the modena model")
     plt.savefig(tree_display_path)
     plt.close()
@@ -68,19 +69,19 @@ print("\n \n \n")
 
 
 # Extract clusters at a threshold : 
-threshold = 180
+threshold = 120
 
 print(f"Extracting clusters at threshold {threshold} ... \n")
 cluster_path = "data/data/clusters/modena/cut/" + str(threshold) + "/"
 
 if path.exists(cluster_path):
     print(f"Cut at threshold {threshold} had already been computed.")
-    clustering = Clustering(mem_path= cluster_path + "mem.csv", size_path= cluster_path + "size.csv", color_path= cluster_path + "color.csv")
+    clustering = Clustering(mem_path= cluster_path + "mem.npy", size_path= cluster_path + "size.npy", color_path= cluster_path + "color.csv")
 else :
     makedirs(cluster_path)
     clustering = percolation_tree.label_of_cut(threshold)
     clustering.get_cluster_colors()
-    clustering.save(mem_path= cluster_path + "mem.csv", size_path= cluster_path + "size.csv", color_path= cluster_path + "color.csv")
+    clustering.save(mem_path= cluster_path + "mem.npy", size_path= cluster_path + "size.npy", color_path= cluster_path + "color.csv")
 
 print(f"Membership table of the clustering with treshold {threshold} :")
 print(clustering.mem_tab)
@@ -101,14 +102,22 @@ else :
     ox.plot.plot_graph(G, node_size = 2, node_color = node_colors, edge_color = "#B0B0B0", bgcolor = '#FFFFFF', show = False, save = True, filepath = cut_display_path, close = True)
 
 
+
 print("\n \n \n")
 
 
 
 # Compute the condensed tree
 min_size = 600
-print(f"Computing the condensed tree with min_cluster_size {min_size} ... \n")
-condensed_tree = percolation_tree.compute_condensed_tree(min_size)
+print(f"Handling the condensed tree with min_cluster_size {min_size} ... \n")
+condensed_tree_path = "data/data/condensed_trees/modena/" + str(min_size) + "/"
+if path.exists(condensed_tree_path):
+    condensed_tree = CondensedTree(path = condensed_tree_path + "/modena_condensed_tree" + str(min_size) + ".npy")
+else : 
+    print("Computing the condensed tree ... \n")
+    makedirs(condensed_tree_path)
+    condensed_tree = percolation_tree.compute_condensed_tree(min_size=min_size)
+    condensed_tree.save(condensed_tree_path + "/modena_condensed_tree" + str(min_size) + ".npy")
 
 
 # Display the condensed tree
@@ -127,17 +136,18 @@ else :
 print("\n \n \n")
 
 
+
 # Get clusters out of stability
 print("Computing stability clusters ... \n")
 cluster_path = "data/data/clusters/modena/stability/" + str(min_size) + "/"
 if path.exists(cluster_path):
     print(f"Stability clusters with min_size {min_size} had already been computed.")
-    clustering = Clustering(mem_path= cluster_path + "mem.csv", size_path= cluster_path + "size.csv", color_path= cluster_path + "color.csv")
+    clustering = Clustering(mem_path= cluster_path + "mem.npy", size_path= cluster_path + "size.npy", color_path= cluster_path + "color.csv")
 else :
     makedirs(cluster_path)
     clustering = condensed_tree.label_of_stability()
     clustering.get_cluster_colors()
-    clustering.save(mem_path= cluster_path + "mem.csv", size_path= cluster_path + "size.csv", color_path= cluster_path + "color.csv")
+    clustering.save(mem_path= cluster_path + "mem.npy", size_path= cluster_path + "size.npy", color_path= cluster_path + "color.csv")
 
 print(f"Membership table of the stability clustering  with min_size {min_size}: ")
 print(clustering.mem_tab)
@@ -166,3 +176,4 @@ else :
     clustering.add_clusters_to_graph(G)
     node_colors = clustering.get_node_colors()
     ox.plot.plot_graph(G, node_size = 2, node_color = node_colors, edge_color = "#B0B0B0", bgcolor = '#FFFFFF', show = False, save = True, filepath = stability_display_path, close = True)
+
